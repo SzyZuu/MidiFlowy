1a87a675cda85d0c0056d753194bcc55e4120281
SzyZu <skwarek116@gmail.com>
2024-04-27 20:13:07 +0200

    chore: MidiDevices Model

---
+++ Models/MidiDevices.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Melanchall.DryWetMidi.Multimedia;
+
+namespace MidiFlowy.Models;
+
+public class MidiDevices
+{
+    private List<InputDevice> InputDevices;
+    private List<OutputDevice> OutputDevices;
+
+    public MidiDevices()
+    {
+        InputDevices = new List<InputDevice>();
+        OutputDevices = new List<OutputDevice>();
+    }
+    
+    public IEnumerable<InputDevice> FindAllInputDevices()
+    {
+        return (IEnumerable<InputDevice>)InputDevices.ToArray();
+    }
+    
+    public IEnumerable<OutputDevice> FindAllOutputDevices()
+    {
+        return (IEnumerable<OutputDevice>)OutputDevices.ToArray();
+    }
+    
+    public void AddInput(IEnumerable<InputDevice> inputDevice)
+    {
+        InputDevices = inputDevice.ToList();
+    }
+
+    public void AddOutput(IEnumerable<OutputDevice> outputDevice)
+    {
+        OutputDevices = outputDevice.ToList();
+    }
+}
