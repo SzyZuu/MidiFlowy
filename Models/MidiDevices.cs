b405ebef75a0d9d7199db93ea2d51cc83bfde1df
SzyZu <skwarek116@gmail.com>
2024-04-27 20:15:24 +0200

    refactor: rename list in Model

--- Models/MidiDevices.cs
+++ Models/MidiDevices.cs
@@ -7,32 +7,32 @@ namespace MidiFlowy.Models;
 
 public class MidiDevices
 {
-    private List<InputDevice> InputDevices;
-    private List<OutputDevice> OutputDevices;
+    private List<InputDevice> _inputDevices;
+    private List<OutputDevice> _outputDevices;
 
     public MidiDevices()
     {
-        InputDevices = new List<InputDevice>();
-        OutputDevices = new List<OutputDevice>();
+        _inputDevices = new List<InputDevice>();
+        _outputDevices = new List<OutputDevice>();
     }
     
     public IEnumerable<InputDevice> FindAllInputDevices()
     {
-        return (IEnumerable<InputDevice>)InputDevices.ToArray();
+        return (IEnumerable<InputDevice>)_inputDevices.ToArray();
     }
     
     public IEnumerable<OutputDevice> FindAllOutputDevices()
     {
-        return (IEnumerable<OutputDevice>)OutputDevices.ToArray();
+        return (IEnumerable<OutputDevice>)_outputDevices.ToArray();
     }
     
     public void AddInput(IEnumerable<InputDevice> inputDevice)
     {
-        InputDevices = inputDevice.ToList();
+        _inputDevices = inputDevice.ToList();
     }
 
     public void AddOutput(IEnumerable<OutputDevice> outputDevice)
     {
-        OutputDevices = outputDevice.ToList();
+        _outputDevices = outputDevice.ToList();
     }
 }
