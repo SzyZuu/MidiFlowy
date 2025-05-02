7f44f31ed2818428001e19c18410be1d786fb4a8
SzyZu <skwarek116@gmail.com>
2025-05-02 11:52:36 +0200

    chore: virtualmidi service

---
+++ Services/TeVirtualMidiService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MidiFlowy.Services;
+
+public class TeVirtualMidiService
+{
+    private Dictionary<string, TeVirtualMIDI> _ports = new();
+
+    public void CreatePort(string name)
+    {
+        if (_ports.ContainsKey(name))
+            throw new Exception("Port with this name already exists");
+        _ports.Add(name, new TeVirtualMIDI(name));
+    }
+
+    public void RemovePort(string name)
+    {
+        if (!_ports.ContainsKey(name))
+            throw new Exception("Couldn't remove port: " + name + ". No port was found with the name");
+        
+        _ports[name].shutdown();
+        _ports.Remove(name);
+    }
+
+    public TeVirtualMIDI GetPortByName(string name)
+    {
+        if (_ports.ContainsKey(name))
+        {
+            return _ports[name];
+        }
+
+        throw new Exception("Couldn't get port: " + name);
+    }
+}
