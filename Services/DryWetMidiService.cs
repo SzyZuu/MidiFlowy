27fbd22f1f625530d60346bd3fec651c9c4e686e
SzyZu <skwarek116@gmail.com>
2025-05-10 10:45:13 +0200

    fix: disconnecting now works properly

--- Services/DryWetMidiService.cs
+++ Services/DryWetMidiService.cs
@@ -9,6 +9,9 @@ public class DryWetMidiService
     private MidiDevicesModel _midiDevicesModel;
     private DevicesConnector _devicesConnector;
     
+    private InputDevice _selectedInput;
+    private OutputDevice[] _selectedOutputs;
+    
     public DryWetMidiService(MidiDevicesModel midiDevicesModel)
     {
         _midiDevicesModel = midiDevicesModel;
@@ -16,34 +19,55 @@ public class DryWetMidiService
 
     public void Reload()
     {
-        InputDevice selectedInput = _midiDevicesModel.GetSelectedInput();
-        OutputDevice[] selectedOutputs = _midiDevicesModel.GetSelectedOutputs();
+        UnsubscribeOutputEvent();
+        
+        _selectedInput = _midiDevicesModel.GetSelectedInput();
+        _selectedOutputs = _midiDevicesModel.GetSelectedOutputs();
 
-        if (selectedInput is null || selectedOutputs is null)
+        if (_selectedInput is null || _selectedOutputs is null)
         {
             Console.WriteLine("In or out is null");
+            if(_devicesConnector is not null)
+                _devicesConnector.Disconnect();
             return;
         }
 
-        foreach (var outputDevice in selectedOutputs)
+        foreach (var outputDevice in _selectedOutputs)
         {
             outputDevice.PrepareForEventsSending();
-            outputDevice.EventSent += (sender, e) =>
-            {
-                Console.WriteLine($"Output from {outputDevice.Name}: {e.Event}");
-            };
+            outputDevice.EventSent += HandleOutputEvent;
         }
 
         if (_devicesConnector is not null)
-            _devicesConnector.Disconnect();
-
-        _devicesConnector = new DevicesConnector(selectedInput, selectedOutputs);
-        _devicesConnector.Connect();
-        selectedInput.StartEventsListening();
-        
-        selectedInput.EventReceived += (sender, e) =>
         {
-            Console.WriteLine($"Input from {selectedInput.Name}: {e.Event}");
-        };
+            _devicesConnector.Disconnect();
+            Console.WriteLine("AREDEVICESCONNECTED: " + _devicesConnector.AreDevicesConnected);
+        }
+
+        _devicesConnector = new DevicesConnector(_selectedInput, _selectedOutputs);
+        _devicesConnector.Connect();
+        _selectedInput.StartEventsListening();
+        
+        // _selectedInput.EventReceived += (sender, e) =>
+        // {
+        //     Console.WriteLine($"Input from {_selectedInput.Name}: {e.Event}");
+        // };
+    }
+
+    private void HandleOutputEvent(object? sender, MidiEventSentEventArgs e)
+    {
+        if(sender is OutputDevice outputDevice)
+            Console.WriteLine($"Output from {outputDevice.Name}: {e.Event}");
+    }
+
+    private void UnsubscribeOutputEvent()
+    {
+        if(_selectedOutputs is null)
+            return;
+
+        foreach (var device in _selectedOutputs)
+        {
+            device.EventSent -= HandleOutputEvent;
+        }
     }
 }
